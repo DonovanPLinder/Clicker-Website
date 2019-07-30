3a8570ee8b08c4602f8f191ad3a4d6ce4d7090bd
Steven <johnson.stevenwilliam@gmail.com>
2019-07-30 08:48:58 -0500

    Refactored code for object oriented design

--- Clicker/javascript/user.js
+++ Clicker/javascript/user.js
@@ -1,4 +1,5 @@
-function User(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
+class User{
+  constructor(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
     this.gold = gold;
     this.fishPerClick = fishPerClick;
     this.fishTotal = fishTotal;
@@ -6,15 +7,15 @@ function User(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
     this.villagerCount = villagerCount;
     this.fisherman = fisherman;
     this.villager = villager; 
-
+  }
     //Function to add a fisherman to the user
-    this.addFisherman = () => {
+    addFisherman() {
         //if the user has enough gold to purchase 
-        if(user.gold >= fisherman.cost){
+        if(this.gold >= fisherman.cost){
             /*add one to the fisherman count and subtract the cost of a fisherman from the user's gold
               returning true to tell main.js that the add was successful*/
-            user.fishermanCount += 1;
-            user.gold -= fisherman.cost;
+            this.fishermanCount += 1;
+            this.gold -= fisherman.cost;
             return true;
         }else{
             return false;
@@ -23,10 +24,10 @@ function User(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
 
     //Function to add a villager to the user
     //Works just like addFisherman except that it cost's fish to purchase a villager
-    this.addVillager = () => {
-        if(user.fishTotal >= villager.cost){
-            user.villagerCount += 1;
-            user.fishTotal -= villager.cost;
+    addVillager() {
+        if(this.fishTotal >= villager.cost){
+            this.villagerCount += 1;
+            this.fishTotal -= villager.cost;
             return true;
           }
           else{
