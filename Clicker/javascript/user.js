60499d3d536ea1cb9b9efd4bceba6d05d986c327
Steven <johnson.stevenwilliam@gmail.com>
2019-07-30 07:20:21 -0500

    added comments for methods of user.js

--- Clicker/javascript/user.js
+++ Clicker/javascript/user.js
@@ -7,8 +7,12 @@ function User(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
     this.fisherman = fisherman;
     this.villager = villager; 
 
+    //Function to add a fisherman to the user
     this.addFisherman = () => {
+        //if the user has enough gold to purchase 
         if(user.gold >= fisherman.cost){
+            /*add one to the fisherman count and subtract the cost of a fisherman from the user's gold
+              returning true to tell main.js that the add was successful*/
             user.fishermanCount += 1;
             user.gold -= fisherman.cost;
             return true;
@@ -17,6 +21,8 @@ function User(gold, fishPerClick, fishTotal, fishermanCount, villagerCount){
         }
     }
 
+    //Function to add a villager to the user
+    //Works just like addFisherman except that it cost's fish to purchase a villager
     this.addVillager = () => {
         if(user.fishTotal >= villager.cost){
             user.villagerCount += 1;
